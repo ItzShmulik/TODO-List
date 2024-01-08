784d3afbb0ce2e2e5b4a21e20657a49be7b3077a
shmulik55 <gshloman@gmail.com>
2024-01-08 20:22:43 +0200

    Added some functionality to the button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addItem_button = document.querySelector("#add-item-button");
 
-addItem_button.onclick = click;
-
 function click(){
     console.log("Clicked the button!");
 }
+
+addItem_button.onclick = click
